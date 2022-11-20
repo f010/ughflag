4e350f7578f289396a1f560490f5fa1219d6bc8d
f010 <mail.fawazabdulla@gmail.com>
2022-11-20 10:51:22 +0530

    add some declarations

---
+++ flag.hpp
@@ -0,0 +1,18 @@
+
+#ifndef FLAG_H
+#define FLAG_H
+
+int *intFlag(const char name, int def, const char usage);
+bool *boolFlag(const char *name, bool val, const char *usage );
+
+#endif
+
+
+int *intFlag (const char name, int def, const char usage) {
+    int *p;
+    // TODO: implement this 
+}
+
+
+
+
---
+++ tmp/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	i := flag.Int64("i", -1, "no usage")
+	i2 := flag.Int64("i2", -1, "the i2 usage text")
+	flag.Usage()
+	flag.Parse()
+
+	fmt.Println("i:", *i, *i2)
+}
